064a5bc63cb26ab7e9ed0f2a447bc3558e3c00e8
WaDuTV <sascha.kaltwasser@gmx.de>
2021-03-12 09:54:56 +0100

    [NEW] HUGE Graphical Update. Game has now comicy look and is cell shadet. Further upgradet to URP. Addes  owkring Post FX.

---
+++ Assets/EconomyManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EconomyManager : MonoBehaviour
+{
+    public static EconomyManager instance { get; private set; }
+
+    public List<GameTypeSO> gameTypes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/NewGameSetup.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.VersionControl;
+using TMPro;
+using System.IO;
+
+public class NewGameSetup : MonoBehaviour
+{
+
+    public TMP_Dropdown mainGenreDropdown;
+    public TMP_Dropdown subGenreDropdown;
+
+    public List<GameTypeSO> gameTypes = null;
+    private List<string> genreOptions = new List<string>(); 
+    private List<string> subGenreOptions = new List<string>();
+
+    private void Awake()
+    {
+        gameTypes = EconomyManager.instance.gameTypes;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mainGenreDropdown.ClearOptions();
+
+        
+        for (int i = 0; i < gameTypes.Count; i++)
+        {
+            string genreOption = gameTypes[i].name;
+            genreOptions.Add(genreOption);
+        }
+
+        mainGenreDropdown.AddOptions(genreOptions);
+        mainGenreDropdown.RefreshShownValue();
+
+        subGenreDropdown.ClearOptions();
+
+        
+        for (int i = 0; i < gameTypes.Count; i++)
+        {
+            string subGenreOption = gameTypes[i].name;
+            if (subGenreOption != mainGenreDropdown.GetComponentInChildren<TMP_Text>().text)
+            {                
+                subGenreOptions.Add(subGenreOption);
+            }
+        }
+
+        subGenreDropdown.AddOptions(subGenreOptions);        
+        subGenreDropdown.RefreshShownValue();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void checkDoubleSubGenres()
+    {
+        subGenreOptions.Clear();
+        for (int i = 0; i < gameTypes.Count; i++)
+        {            
+            string subGenreOption = gameTypes[i].name;            
+            if (subGenreOption != mainGenreDropdown.GetComponentInChildren<TMP_Text>().text)
+            {
+                subGenreOptions.Add(subGenreOption);
+            }
+
+        }
+        subGenreDropdown.ClearOptions();
+        subGenreDropdown.AddOptions(subGenreOptions);
+        subGenreDropdown.RefreshShownValue();
+    }
+        
+}
